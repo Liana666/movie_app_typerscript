2e01e922fd14cf59ae4622c6bd21bc0d4d46c225
Liana666 <liana.nikitina98@gmail.com>
2021-11-25 22:41:53 +0300

    burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../../img/logo.png";
+import eye from "../../img/eye.png";
 
 import { NavLink } from "react-router-dom";
 import { useLocation } from 'react-router-dom'
@@ -11,8 +12,12 @@ import header from "./Header.module.css";
 import SearchContainer from "../Main/Search/SearchContainer";
 
 const Header = () => {
-    let location = useLocation()
-    console.log(location);
+    let [isTrue, setIsTrue] = useState(false);
+    let location = useLocation();
+
+    const changeMenuIcon = () => {
+        setIsTrue(true);
+    }
 
     return (
         <>
@@ -29,6 +34,27 @@ const Header = () => {
                     <NavLink className={header.link} to="/profile/all-movies">Избранное</NavLink>
                     {location.pathname === "/movie" ? <SearchContainer /> : null}
                 </div>
+
+
+                <div className={header.mob_header}>
+                    <div className={header.burger} onclick={changeMenuIcon}>
+                        {isTrue ? <img className={header.burger_active} src={eye} />
+                            : <>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </>
+                        }
+                    </div>
+                    <div className={isTrue ? header.link_wrapper2_active : header.link_wrapper2}>
+                        <NavLink to="/movie">Фильмы</NavLink>
+                        <NavLink className={header.link} to="/profile/all-movies">Избранное</NavLink>
+                        <div>
+                            {location.pathname === "/movie" ? <SearchContainer /> : null}
+                        </div>
+
+                    </div>
+                </div>
             </HeaderWrapper>
         </>
     )
@@ -36,6 +62,7 @@ const Header = () => {
 
 
 const HeaderWrapper = styled.header`
+    position: relative;
     padding: 15px 15px;
 
     display: flex;
