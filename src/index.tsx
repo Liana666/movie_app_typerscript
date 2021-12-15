b07de355ec6dd11c3c6fe00a25757fc8c38148a4
Liana666 <78962820+Liana666@users.noreply.github.com>
2021-12-15 09:53:59 +0300

    BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,16 @@ import './index.css';
 import ReactDOM from 'react-dom';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
